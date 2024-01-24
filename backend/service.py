cd117bacfa6481eec3c3f96e5f64b776407e5a30
mosazhaw <adrian.moser@zhaw.ch>
2024-01-24 10:11:50 +0100

    svelte ui

---
+++ backend/service.py
@@ -0,0 +1,44 @@
+# flask --debug --app service run
+
+import pickle
+import pandas as pd
+from pathlib import Path
+
+# load it again
+file_path = Path(".", "../model/", "GradientBoostingRegressor.pkl")
+with open(file_path, 'rb') as fid:
+    gbr = pickle.load(fid)
+
+print("*** DEMO ***")
+downhill = 300
+uphill = 700
+length = 10000
+max_elevation = 1200
+print("Downhill: " + str(downhill))
+print("Uphill: " + str(uphill))
+print("Length: " + str(length))
+demoinput = [[downhill,uphill,length,max_elevation]]
+demodf = pd.DataFrame(columns=['downhill', 'uphill', 'length_3d', 'max_elevation'], data=demoinput)
+demooutput = gbr.predict(demodf)
+time = demooutput[0]
+
+import datetime
+print("Our Model: " + str(datetime.timedelta(seconds=time)))
+
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    downhill = request.args.get('downhill', default = 0, type = int)
+    uphill = request.args.get('uphill', default = 0, type = int)
+    length = request.args.get('length', default = 0, type = int)
+
+    demoinput = [[downhill,uphill,length,0]]
+    demodf = pd.DataFrame(columns=['downhill', 'uphill', 'length_3d', 'max_elevation'], data=demoinput)
+    demooutput = gbr.predict(demodf)
+    time = demooutput[0]
+
+    return str(datetime.timedelta(seconds=time))
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	let count = 0;
+
+	function increment() {
+		count++;
+	}
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
