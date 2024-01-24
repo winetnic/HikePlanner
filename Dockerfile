51e457c73357dd715b852861e81ae7f0205490b3
mosazhaw <adrian.moser@zhaw.ch>
2024-01-24 13:36:28 +0100

    svelte added

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Usage
+# docker build -t mosazhaw/hikeplanner .
+# docker run --name hikeplanner -e AZURE_STORAGE_CONNECTION_STRING='***' -p 9001:5000 -d mosazhaw/hikeplanner
+
+FROM python:3.12.1
+
+# Copy Files
+WORKDIR /usr/src/app
+COPY backend/service.py backend/service.py
+COPY frontend/build frontend/build
+
+# Install
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+# Docker Run Command
+EXPOSE 5000
+ENV FLASK_APP=/usr/src/app/backend/service.py
+CMD [ "python", "-m" , "flask", "run", "--host=0.0.0.0"]
--- backend/service.py
+++ backend/service.py
@@ -1,10 +1,48 @@
 # flask --debug --app service run
 
+import os
 import pickle
 import pandas as pd
 from pathlib import Path
+from azure.storage.blob import BlobServiceClient
 
 # load it again
+print(os.environ)
+
+if 'AZURE_STORAGE_CONNECTION_STRING' in os.environ:
+    azureStorageConnectionString = os.environ['AZURE_STORAGE_CONNECTION_STRING']
+    blob_service_client = BlobServiceClient.from_connection_string(azureStorageConnectionString)
+
+    containers = blob_service_client.list_containers(include_metadata=True)
+    for container in containers:
+        existingContainerName = container['name']
+        print(existingContainerName, container['metadata'])
+        if existingContainerName.startswith("hikeplanner-model"):
+            parts = existingContainerName.split("-")
+            print(parts)
+            suffix = 1
+            if (len(parts) == 3):
+                newSuffix = int(parts[-1])
+                if (newSuffix > suffix):
+                    suffix = newSuffix
+
+    container_client = blob_service_client.get_container_client("hikeplanner-model-" + suffix)
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print("\t" + blob.name)
+
+    # Download the blob to a local file
+    # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
+    download_file_path = os.path.join(".", "GradientBoostingRegressor.pkl")
+    print("\nDownloading blob to \n\t" + download_file_path)
+
+    with open(file=download_file_path, mode="wb") as download_file:
+         download_file.write(container_client.download_blob(blob.name).readall())
+
+
+else:
+    print("AZURE_STORAGE_CONNECTION_STRING not set")    
+
 file_path = Path(".", "../model/", "GradientBoostingRegressor.pkl")
 with open(file_path, 'rb') as fid:
     gbr = pickle.load(fid)
@@ -26,15 +64,17 @@ import datetime
 print("Our Model: " + str(datetime.timedelta(seconds=time)))
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
 
+app = Flask(__name__, static_url_path='/', static_folder='../frontend/build')
+
 @app.route("/")
-def home():
-    return "backend"
+def indexPage():
+     return send_file("../frontend/build/index.html")  
 
 @app.route("/api/predict")
 def hello_world():
